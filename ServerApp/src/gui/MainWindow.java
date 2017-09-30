95a4d8201ebd8c128eabcf81290ebdaf23a91933
samuelbak <samuele1988@gmail.com>
2017-09-30 18:15:21 +0200

    Add javafx panel

--- ServerApp/src/gui/MainWindow.java
+++ ServerApp/src/gui/MainWindow.java
@@ -1,48 +1,72 @@
 package gui;
 
+import javafx.application.Platform;
+import javafx.embed.swing.JFXPanel;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
-
 import src.ServerConnectionListener;
+import java.awt.BorderLayout;
+import javax.swing.JPanel;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.JButton;
 
 public class MainWindow {
 
-	private JFrame frmServerapp;
 
-	/**
-	 * Launch the application.
-	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
-				try {
-					MainWindow window = new MainWindow();
-					window.frmServerapp.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
+				initAndShowGUI();
 				//my code
 				ServerConnectionListener listener = new ServerConnectionListener(12345);
 			}
 		});
 	}
+	
+	private static void initAndShowGUI() {
+        // This method is invoked on the EDT thread
+        JFrame frame = new JFrame();
+        frame.getContentPane().setLayout(new MigLayout("", "[367px,grow]", "[275.00px,grow]"));
+        final JFXPanel fxPanel = new JFXPanel();
+        frame.getContentPane().add(fxPanel, "cell 0 0,grow");
+        frame.setSize(383, 325);
+        frame.setVisible(true);
+        frame.setTitle("ServerApp");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-	/**
-	 * Create the application.
-	 */
-	public MainWindow() {
-		initialize();
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                initFX(fxPanel);
+            }
+       });
 	}
+	
+	private static void initFX(JFXPanel fxPanel) {
+        // This method is invoked on the JavaFX thread
+        Scene scene = createScene();
+        fxPanel.setScene(scene);
+    }
+	
+	private static Scene createScene() {
+        Group  root  =  new  Group();
+        Scene  scene  =  new  Scene(root, Color.ALICEBLUE);
+        Text  text  =  new  Text();
+        
+        text.setX(40);
+        text.setY(100);
+        text.setFont(new Font(25));
+        text.setText("Welcome JavaFX!");
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frmServerapp = new JFrame();
-		frmServerapp.setTitle("ServerApp");
-		frmServerapp.setBounds(100, 100, 450, 300);
-		frmServerapp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
+        root.getChildren().add(text);
+
+        return (scene);
+    }
 
 }
