21f0b5477e4b7833d5bccdcc5236a055b9a21a82
Samuele Bacchetta <samuele1988@gmail.com>
2017-10-02 11:08:57 +0200

    Moved to Fx and added map

---
+++ ClientApp/src/src/ClientApp.java
@@ -0,0 +1,34 @@
+package src;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+
+public class ClientApp extends Application{
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		try {
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("../gui/MainWindow.fxml"));
+			Scene scene = new Scene(root, 400, 400);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			primaryStage.setOnCloseRequest(e -> Platform.exit());
+			initializeApp();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void initializeApp() {
+		new ConnectionHandler("localhost", 12345);
+	}
+}
---
+++ ServerApp/src/gui/MainWindowController.java
@@ -0,0 +1,51 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import com.lynden.gmapsfx.GoogleMapView;
+import com.lynden.gmapsfx.MapComponentInitializedListener;
+import com.lynden.gmapsfx.javascript.object.GoogleMap;
+import com.lynden.gmapsfx.javascript.object.LatLong;
+import com.lynden.gmapsfx.javascript.object.MapOptions;
+import com.lynden.gmapsfx.javascript.object.MapTypeIdEnum;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import src.ServerConnectionListener;
+
+public class MainWindowController implements Initializable, MapComponentInitializedListener{
+	
+	@FXML
+    private GoogleMapView mapView;
+    
+    public static GoogleMap map;
+
+	
+	public void appStart() {
+		new ServerConnectionListener(12345);
+	}
+
+	@Override
+	public void mapInitialized() {
+		MapOptions mapOptions = new MapOptions();
+        
+        mapOptions.center(new LatLong(47.6097, -122.3331))
+                .mapType(MapTypeIdEnum.ROADMAP)
+                .overviewMapControl(false)
+                .panControl(false)
+                .rotateControl(false)
+                .scaleControl(false)
+                .streetViewControl(false)
+                .zoomControl(false)
+                .zoom(12);
+                   
+        map = mapView.createMap(mapOptions);	
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		mapView.addMapInializedListener(this);
+	}
+
+}
